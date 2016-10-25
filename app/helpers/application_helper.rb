39aa8c1907149f089f76f694327153c5b5a8db92
Ngoc Truong <ntruong@protonmail.com>
2016-10-25 14:50:42 +0200

    change layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Teile Inhalte aus dem Internet"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
