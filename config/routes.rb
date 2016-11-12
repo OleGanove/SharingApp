9cd5db7433cc9271dd25060004d3f8c22975032f
Ngoc Truong <ntruong@protonmail.com>
2016-11-12 20:02:06 +0100

    Add posts#index styling

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get '/impressum', to: 'static_pages#impressum'
-  get '*path' => redirect('/')
+  #get '*path' => redirect('/')
 end
