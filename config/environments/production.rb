594251a0a85a6d328d38def392436418f2388271
Ngoc Truong <ntruong@protonmail.com>
2016-12-19 20:12:22 +0100

    Put Paperclip default options into production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.paperclip_defaults = {
+    
+    Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+    Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
+
     storage: :s3,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
@@ -92,5 +97,6 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     }
+
   }
 end
