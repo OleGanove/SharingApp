1b867cbda1693068a0b66106170fd235d83a5d52
Ngoc Truong <ntruong@protonmail.com>
2017-01-02 18:27:03 +0100

    changed paperclip hash in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.paperclip_defaults = {
-    Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-    Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
+    :url => ':s3_domain_url', 
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :s3_host_name => 's3-eu-central-1.amazonaws.com',
 
     storage: :s3,
     s3_credentials: {
