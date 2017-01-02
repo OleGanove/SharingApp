89286ad69e8a36ca74d9b93a0d36707f755b0ae4
Ngoc Truong <ntruong@protonmail.com>
2017-01-02 17:41:48 +0100

    changed production.rb syntax to fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,18 +85,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.paperclip_defaults = {
-    
     Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
     Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
     Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
 
-    storage: :s3,
+    :storage => s3,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     }
-
   }
 end
