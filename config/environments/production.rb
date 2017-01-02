b31c07590a0cdd3c6cc61cadd9a96d03e553e5b9
Ngoc Truong <ntruong@protonmail.com>
2017-01-02 17:42:40 +0100

    changed production.rb syntax to fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
     Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
 
-    :storage => s3,
+    :storage => :s3,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
