57a944e072e038db58ce36a7c95093ca25836296
Ngoc Truong <ntruong@protonmail.com>
2017-01-02 17:48:07 +0100

    deleted updated time when login to solve heroku problem

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,11 +2,11 @@ class Users::SessionsController < Devise::SessionsController
 
   def create
     super do |user| 
-      user.randomized_fposts.delete_all
-      Fpost.all.each do |fp|
-        resource.randomized_fposts.new(fpost: fp)
-      end
-      user.save
+      #user.randomized_fposts.delete_all
+      #Fpost.all.each do |fp|
+      #  resource.randomized_fposts.new(fpost: fp)
+      #end
+      #user.save
     end
   end
 end
