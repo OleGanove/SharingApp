606382602fdaf764084e0d8ad4276ae1fcf6cb86
Ngoc Truong <ntruong@protonmail.com>
2017-01-10 21:36:13 +0100

    Changed users#show page and disallow access to other pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :only_see_own_page, only: [:show]
 
   def show
     begin
       @user = User.find(params[:id])
+      @fakeposts = Fpost.joins(:randomized_fposts).where(randomized_fposts: {user_id: @user.id}).select("fposts.*, randomized_fposts.fake_time")
       @posts = @user.posts
+    
+      @liked_fakeposts = @fakeposts.joins(:flikes).where(flikes: {user_id: @user.id }).select("fposts.*, randomized_fposts.fake_time") + @posts
+      @liked_fakeposts = @liked_fakeposts.sort_by(&:fake_time)
+
+      @likes = @user.flikes + @user.likes
+
     rescue ActiveRecord::RecordNotFound
       redirect_to root_path
     end
   end
 
+  private
+
+  def only_see_own_page
+    @user = User.find(params[:id])
+
+    if current_user != @user
+      redirect_to root_path, notice: "Tut mir Leid, aber du hast keinen Zugang auf die Seite."
+    end
+  end
 end
